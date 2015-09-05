7a431b2b6ae3169dc56c91c2045446171e83b331
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-09-05 13:50:42 -0400

    Issues route

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return render_template("home.html")
 
+@app.route("/issues")
+def issues():
+	return render_template("issues.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
