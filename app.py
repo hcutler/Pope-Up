4918d6361ce7f9911f0086d825d1749f14b8111a
Hannah Cutler New <Hannah-Cutler@Hannahs-MacBook-Pro-2.local>
2015-09-05 15:27:50 -0400

    fixed app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from form import IssuesForm, UserDataForm
+
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route("/")
 def hello():
-    return render_template("home.html")
+	form = IssuesForm()
+	return render_template("home.html", form=form)
 
 @app.route("/issues")
 def issues():
